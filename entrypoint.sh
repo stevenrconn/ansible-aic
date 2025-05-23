f3f47e1bb556b632c88e6a7f4cc7bd274984da14
steven conn <steven.conn@gmail.com>
2025-05-23 16:34:49 -0400

    work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
 command=ansible
+
+if [[ $# > 0 ]]; then
+    case "$1" in
+        ansible)
+            shift
+            ;;
+        playbook | vault | doc | pull | console | config )
+            command="ansible-$1"
+            shift
+            ;;
+    esac
+fi
+
+if [[ $# > 0 ]]; then
+    echo "$command $@"
+else
+    $command --help
+fi
